fa801060982e09ac1730c1671cf738c011d3a500
AlexMollohan <ajmollohan@gmail.com>
2017-05-05 09:46:58 -0400

    moved test functionality into movie events.

--- assets/scripts/movies/events.js
+++ assets/scripts/movies/events.js
@@ -5,8 +5,15 @@ const getFormFields = require(`../../../lib/get-form-fields`)
 const api = require('./api')
 const ui = require('./ui')
 
-const addHandlers = () => {
+const test = function (event) {
+  event.preventDefault()
+  let data = getFormFields(this)
+  data = data.key
+  console.log(data)
+}
 
+const addHandlers = () => {
+  $('.fave').on('click', test)
 }
 
 module.exports = {
