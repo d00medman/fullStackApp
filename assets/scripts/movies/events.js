19946b9662e87c968809f092da93d1317550fcf2
AlexMollohan <ajmollohan@gmail.com>
2017-05-05 10:26:01 -0400

    Running tests to discern source of problem
    Updated HTML appropriately
    
    Built burndown harness to test form field submission
    
    Problem appears to be on the server side.

--- assets/scripts/movies/events.js
+++ assets/scripts/movies/events.js
@@ -8,13 +8,20 @@ const ui = require('./ui')
 const onCreate = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
+  console.log(data)
   api.create(data)
     .then(ui.createSuccess)
     .catch(ui.createFailure)
 }
 
+const test = function (event) {
+  event.preventDefault()
+  console.log(getFormFields(this))
+}
+
 const addHandlers = () => {
   $('.fave-submit').on('submit', onCreate)
+  $('.test-submit').on('submit', test)
 }
 
 module.exports = {
